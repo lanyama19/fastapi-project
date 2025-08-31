6b166248faf4cdf83806c442e97a72c221a26f97
frankma <laneyamar@gmail.com>
2025-08-31 23:01:04 +0800

    add delete functions; change code with http exception code

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Response, status
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -15,6 +15,7 @@ class Post(BaseModel):
     rating: Optional[int] = None # Optional value
 
 
+# Hard coded data for demo, stored in memory
 my_posts = [{"title":"title of post 1", "content": "content of post 1", "id": 1},
             {"title":"favorite foods", "content": "I like pizza", "id": 2}]
 
@@ -23,6 +24,12 @@ def find_post(id):
     for p in my_posts:
         if p['id'] == id:
             return p
+
+
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
 
 
 @app.get("/")
@@ -35,7 +42,7 @@ def get_posts():
     return {"data": my_posts}
 
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
     # print(post.rating)
     # print(post.model_dump())  # .dict() method is deprecated
@@ -58,8 +65,22 @@ def get_post(id: int, response: Response):
     post = find_post(id)
     # print(post)
     if not post:
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return {"message": f"post with id: {id} was not found"}
+        # --------- handling no-exiting post ids: method 1-----------
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"message": f"post with id: {id} was not found"}
+        # --------- handling no-exiting post ids: method 2-----------
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} was not found")
     return {"post details": post}
 
 
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    # find the index in the array that has required id
+    # my_posts.pop(index)
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} does not exist")
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT) # 204 not send any message back
