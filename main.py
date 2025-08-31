4280d9d41a4c60a634fd6820e1bce8cd888cafa7
frankma <laneyamar@gmail.com>
2025-08-31 19:23:40 +0800

    ready for CRUD functions

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
+
 
 app = FastAPI()
 
@@ -13,6 +15,10 @@ class Post(BaseModel):
     rating: Optional[int] = None # Optional value
 
 
+my_posts = [{"title":"title of post 1", "content": "content of post 1", "id": 1},
+            {"title":"favorite foods", "content": "I like pizza", "id": 2}]
+
+
 @app.get("/")
 async def root():
     return {"message": "Welcome to my new api"}
@@ -20,12 +26,22 @@ async def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
 
 
-@app.post("/creatposts")
-def create_posts(new_post: Post):
-    print(new_post.rating)
-    print(new_post.dict())
-    return  {"data": "new post"}
+@app.post("/posts")
+def create_posts(post: Post):
+    # print(post.rating)
+    # print(post.model_dump())  # .dict() method is deprecated
+    post_dict = post.model_dump()
+    post_dict['id'] = randrange(0, 100000000)
+    my_posts.append(post_dict)
+    return  {"data": post_dict}
 # title str, content str
+
+
+@app.get("/posts/{id}")
+def get_post(id):
+    print(id)
+    return {"post details": f"Here is post {id}"}
+    
