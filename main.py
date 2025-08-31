f165e3a107bafaee2278ec8f6046a63d6d7ee65b
frankma <laneyamar@gmail.com>
2025-08-31 22:24:44 +0800

    update post function for error handling

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -17,6 +17,12 @@ class Post(BaseModel):
 
 my_posts = [{"title":"title of post 1", "content": "content of post 1", "id": 1},
             {"title":"favorite foods", "content": "I like pizza", "id": 2}]
+
+
+def find_post(id):
+    for p in my_posts:
+        if p['id'] == id:
+            return p
 
 
 @app.get("/")
@@ -40,8 +46,20 @@ def create_posts(post: Post):
 # title str, content str
 
 
+@app.get("/posts/latest")
+def get_latest_post():
+    post = my_posts[len(my_posts)-1]
+    return {"details": post}
+
+
 @app.get("/posts/{id}")
-def get_post(id):
+def get_post(id: int, response: Response):
     print(id)
-    return {"post details": f"Here is post {id}"}
-    
+    post = find_post(id)
+    # print(post)
+    if not post:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"message": f"post with id: {id} was not found"}
+    return {"post details": post}
+
+
