c7fba9abde4e7a1a2e32d0f9827516962a6ac342
frankma <laneyamar@gmail.com>
2025-09-04 21:28:36 +0800

    add password hashing verify function

--- app/utils.py
+++ app/utils.py
@@ -4,5 +4,10 @@ from passlib.context import CryptContext
 # Set default hashing algorithm for password encrypt
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+
 def hash(password: str):
     return pwd_context.hash(password)
+
+
+def verify(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
