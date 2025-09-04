eb51212e7a797398f69e868cfbf5a2b18e8307de
frankma <laneyamar@gmail.com>
2025-09-04 15:07:36 +0800

    updated user get method;

--- app/main.py
+++ app/main.py
@@ -2,20 +2,18 @@ from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional, List
-from passlib.context import CryptContext
 from random import randrange
 import psycopg
 from psycopg.rows import dict_row
 import time
 from sqlalchemy.orm import Session
-from . import models
+from . import models, schemas, utils
 from .database import engine, get_db
-from . import schemas
+
 
 # Create databse for postgres
 models.Base.metadata.create_all(bind=engine)
-# Set default hashing algorithm for password encrypt
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 app = FastAPI()
 
@@ -152,7 +150,7 @@ def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)
 def creat_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
     #hash the password - user.password
-    hashed_password = pwd_context.hash(user.password)
+    hashed_password = utils.hash(user.password)
     user.password = hashed_password
     new_user = models.User(**user.model_dump())
     db.add(new_user)
@@ -160,3 +158,13 @@ def creat_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
 
     return new_user
+
+
+@app.get('/users/{id}', response_model=schemas.UserOut)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id: {id} does not exist.")
+    
+    return user
