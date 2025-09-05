ce4acac6bdb9a631482319ce1060b661a7cf29d3
frankma <laneyamar@gmail.com>
2025-09-05 18:22:56 +0800

    add usr verification function with JWT exceptions

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,7 +1,11 @@
 from jose import JWTError, jwt
 import copy
 from datetime import datetime, timedelta
+from . import schemas
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 
+oath2_schema = OAuth2PasswordBearer(tokenUrl='login')
 
 # SECRET_KEY
 SECRET_KEY = "3ae82acd8da07595d8b20cffaf1e40827b18a02742750c7bc50806a234700939"
@@ -14,9 +18,31 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 def create_access_token(data: dict):
     to_encode = copy.deepcopy(data)
     # add expiration data to the JWT
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    
+
     return encoded_jwt
+
+
+def verify_access_token(token: str, credentials_exception):
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        id: str = payload.get("user_id")
+
+        if id is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(id = id)
+    except JWTError:
+        raise credentials_exception
+    
+    return token_data
+    
+
+def get_current_user(token: str = Depends(oath2_schema)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                          detail=f"Could not validate credentials", headers={"WWW-Authenticate":"Bearer"})
+    
+    return verify_access_token(token, credentials_exception)
