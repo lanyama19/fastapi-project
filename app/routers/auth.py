78e2f25e1656e30067973ad31faf54f944665550
frankma <laneyamar@gmail.com>
2025-09-05 18:29:15 +0800

    changed http exception code; add usage of fastapi Oauthpassword lib

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,24 +1,26 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from ..database import get_db
-from .. import schemas, models, utils
+from .. import schemas, models, utils, oauth2
 
 router = APIRouter(tags=['Authentication'])
 
 
 @router.post('/login')
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
 
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, 
                             detail=f"Invalid credentials, please try again.")
     
     if not utils.verify(user_credentials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, 
                             detail="Invalid username or password.")
 
     # create a token
+    access_token = oauth2.create_access_token(data = {"user_id": user.id})
     # return the token to users
-    return {"token": "example token"}
+    return {"access_token": access_token, "token_type": "bearer"}
