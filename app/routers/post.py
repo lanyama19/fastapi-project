36b072c191bed717c4fd8772b3bde061a455345b
frankma <laneyamar@gmail.com>
2025-09-07 08:54:36 +0800

    Set rules for users can only delete or update their own post

--- app/routers/post.py
+++ app/routers/post.py
@@ -22,7 +22,7 @@ def get_posts(db: Session = Depends(get_db)):
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     ## Run Regular SQL Query to Insert data into the db
     # cur.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING* """, 
     #             (post.title, post.content, post.published))
@@ -30,8 +30,8 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), user_i
     # conn.commit() # push and stage changes
     ## With the ORM:
     # new_post = models.Post(title = post.title, content = post.content, published = post.published)  # Bad practice for having many fields
-    print(user_id)
-    new_post = models.Post(**post.model_dump())
+    print(current_user.email)
+    new_post = models.Post(owner_id = current_user.id, **post.model_dump())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
@@ -40,7 +40,8 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), user_i
 
 
 @router.get("/{id}", response_model=schemas.Post)
-def get_post(id: int, response: Response, db: Session = Depends(get_db)):
+def get_post(id: int, response: Response, db: Session = Depends(get_db), 
+             current_user: int = Depends(oauth2.get_current_user)):
     # # Regular SQL query for getting one entry
     # cur.execute("""SELECT * FROM posts WHERE id = %s""",(str(id),))
     # post = cur.fetchone()
@@ -58,7 +59,7 @@ def get_post(id: int, response: Response, db: Session = Depends(get_db)):
 
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     # cur.execute("""DELETE FROM posts WHERE id = %s returning* """,(str(id),))
     # deleted_post = cur.fetchone()
     # conn.commit()
@@ -68,13 +69,17 @@ def delete_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(o
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} does not exist")
     
+    if post.first().owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail=f"Not authorized to perform requested action.")
+    
     post.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT) # 204 not send any message back
 
 
 @router.put("/{id}", response_model=schemas.Post)
-def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     # # Using regular querys to do updates on db
     # cur.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING* """, 
     #             (post.title, post.content, post.published, str(id)))
@@ -88,6 +93,10 @@ def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} does not exist")
     
+    if post_query.first().owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail=f"Not authorized to perform requested action.")
+
     post_query.update(post.model_dump(), synchronize_session=False)
     db.commit()
     return post_query.first()
